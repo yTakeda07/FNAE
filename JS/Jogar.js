8cb7db779fb28c9d3498e3921f4eb6287086ed40
yTakeda07 <isaac.rogovski@gmail.com>
2025-08-14 16:48:30 -0300

    0.3.2
    Teste com câmeras no formato.

--- JS/Jogar.js
+++ JS/Jogar.js
@@ -12,20 +12,27 @@ body.style.backgroundImage = "url('../IMG/FDAE.jpg')";
 
 
 const main = `
-
-<input type="button" value="--" id="abrirCamera">
+<div id="centralizar">
+<input type="button" value="------------------------------------" id="abrirCamera">
+</div>
 <div class="modal-overlay" id="modalOverlay">
   <div class="modal">
-    <input type="button" value="C1" id="C1" onclick="verificarCamera(1)"><br>
-    <input type="button" value="C2" id="C2" onclick="verificarCamera(2)"><br>
-    <input type="button" value="C3" id="C3" onclick="verificarCamera(3)"><br>
-    <input type="button" value="C4" id="C4" onclick="verificarCamera(4)"><br>
-    <input type="button" value="C5" id="C5" onclick="verificarCamera(5)"><br>
     <img src="../IMG/0.jpg" alt="Camera 1" id="CI1" class="cameras">
     <img src="../IMG/7.jpg" alt="Camera 2" id="CI2" class="cameras">
     <img src="../IMG/11.jpg" alt="Camera 3" id="CI3" class="cameras">
     <img src="../IMG/15.jpg" alt="Camera 4" id="CI4" class="cameras">
     <img src="../IMG/19.jpg" alt="Camera 5" id="CI5" class="cameras">
+    <p>Camera 1</p>
+      <div id="cameraContainer">
+    
+    <input type="button" value="C1" id="C1" onclick="verificarCamera(1)"><br>
+    <input type="button" value="C2" id="C2" onclick="verificarCamera(2)"><br>
+    <input type="button" value="C3" id="C3" onclick="verificarCamera(3)"><br>
+    <input type="button" value="C4" id="C4" onclick="verificarCamera(4)"><br>
+    <input type="button" value="C5" id="C5" onclick="verificarCamera(5)"><br>
+    <input type="button" value="C6" id="C6" onclick="verificarCamera(6)"><br>
+    <input type="button" value="C7" id="C7" onclick="verificarCamera(7)"><br>
+  </div>
   </div>
 </div>
 `;
@@ -57,24 +64,6 @@ verificarCamera.src = "../JS/verificarCamera.js";
 document.body.appendChild(verificarCamera);
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
